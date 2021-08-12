e17b427d7c81c72a5996c29bef0cea9ded561553
alerekaw <aleksandra.rekawek95@gmail.com>
2021-08-12 13:33:36 -0700

    outline2

--- src/components/container/list/list.js
+++ src/components/container/list/list.js
@@ -4,7 +4,7 @@ import styles from './list.module.css';
 function List() {
     return (
         <div className={styles.list}>
-            <button className={styles.button}> x </button>
+            <button className={styles.button}>x</button>
             &nbsp;
             Here will be a list of items
     
