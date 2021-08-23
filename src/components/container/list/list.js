0b1ccedcf4d6105b73abb0a8fd586a061d8d1f02
alerekaw <aleksandra.rekawek95@gmail.com>
2021-08-23 12:24:56 -0700

    implementation delete function

--- src/components/container/list/list.js
+++ src/components/container/list/list.js
@@ -7,7 +7,7 @@ const List = () => {
     const [itemsList, setItemsList] = useState([]);
     const [ userInput, setUserInput ] = useState();
     useEffect(() => {
-        setItemsList(["iphone", "mac", "laptop"]);
+        setItemsList(["phone"]);
 
     }, [])
      
@@ -19,6 +19,13 @@ const List = () => {
 
      const handleChange = (e) => {
         setUserInput(e.currentTarget.value)
+    }
+
+    const deleteItem = (e) =>{
+        let copy = [...itemsList]
+        const index = copy.indexOf(e.currentTarget.id)
+        if (index > -1) {copy.splice(index, 1)}
+        setItemsList(copy);
     }
 
     return (
@@ -35,12 +42,15 @@ const List = () => {
         <button 
             onClick={(e) => addItem(e)}
             className={styles.button}> Add product </button>
+        
         </div>
     </div>
     
         <ul className={styles.list}>
             {itemsList.map((el, idx) => (
-                <li key={idx}> <button className={styles.button}> x </button> {el}  </li>
+                <li key={idx}> 
+                <button>
+             x </button> {el}  <button id={el} onClick={deleteItem}>Delete</button></li>
             ))}
            
 </ul>
